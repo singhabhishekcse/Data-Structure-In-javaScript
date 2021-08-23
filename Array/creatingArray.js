fd31aa76a89f6a4cad80fe86dad0db458f4bf601
singhabhishekcse <singhabhishekcse@yahoo.com>
2021-08-23 21:47:46 +0530

    creating array folder

---
+++ Array/creatingArray.js
@@ -0,0 +1,25 @@
+// 1:- creating array 
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+console.log(numbers);
+
+
+// 2:- second method to create array
+
+const names = new Array();
+names[0]="Abhishek";
+names[1]="Ram";
+names[2]="Shayam";
+names[3]="Lakchaman";
+names[4]="om";
+
+console.log(names);
+
+// 3:- third method to creating array 
+
+const nam =[];
+nam.push("ram");
+nam.push("abhi");
+nam.push("ravi");
+nam.push("aman");
+console.log(nam);
